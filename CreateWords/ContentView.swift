4ff90c2abd567a916ce8eecda7c3405aa0f8d3b8
Anton Makarov <makarov070786@mail.ru>
2022-07-15 16:12:11 +0300

    Initial Commit

---
+++ CreateWords/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreateWords
+//
+//  Created by Антон Макаров on 15.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreateWords/CreateWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateWordsApp.swift
+//  CreateWords
+//
+//  Created by Антон Макаров on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CreateWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
